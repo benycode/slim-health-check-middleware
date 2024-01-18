d7d286e3a79baa453245e240458cc7c97aa40df0
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2024-01-18 22:17:21 +0200

    Update HealthCheckEndpointMiddleware.php

--- src/HealthCheckEndpointMiddleware.php
+++ src/HealthCheckEndpointMiddleware.php
@@ -1,39 +1,38 @@
 <?php
-
+ 
 declare(strict_types=1);
-
+ 
 namespace BenyCode\Slim\Middleware;
-
+ 
+use Slim\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
+ 
 final class HealthCheckEndpointMiddleware implements MiddlewareInterface
 {
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface
     {
         $uri = $request
             ->getUri()
             ->getPath()
-          ;
-
-        if ($uri === '/_health') {
-            $response = $handler
-                ->handle($request)
-		;
-
+        ;
+ 
+        if ('/_health' === $uri) {
+            $response = new Response;
+            $response
+                ->getBody()
+                ->write('OK')
+            ;
+ 
             return $response
                 ->withStatus(200)
-                ->withJson(
-			[
-				'status' => 'OK',
-			]
-		    	);
+            ;
         }
-
+ 
         return $handler
-		->handle($request)
-		;
+            ->handle($request)
+        ;
     }
 }
